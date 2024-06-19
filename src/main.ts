27b070662ad73090a084ee6b942c636df1295ef1
mikhail <mikhail@sudarkin.com>
2024-06-19 16:45:19 +0500

    change app port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get<ConfigService>(ConfigService);
+  const port = Number(config.get<number>('SERVER_PORT'));
+  await app.listen(port);
 }
 bootstrap();
